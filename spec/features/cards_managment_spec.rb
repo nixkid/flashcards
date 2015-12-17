2bdeb9ed158f1389a471c5b6dcfe1d17a4db91ed
Sergey <nixkid16@gmail.com>
2015-12-17 10:28:22 +0000

    Adding some tests

--- spec/features/cards_managment_spec.rb
+++ spec/features/cards_managment_spec.rb
@@ -13,19 +13,19 @@ describe "Card management", type: :feature do
   it "creates a new card" do
     visit new_card_path
 
-    fill_in 'card_original_text', with: "My Card"
-    fill_in 'card_translated_text', with: "Моя карта"
+    fill_in 'card_original_text', with: "Home"
+    fill_in 'card_translated_text', with: "Дом"
     click_button "Сохранить карточку"
 
     expect(page).to have_text("Новая карточка создана")
   end
   
-  # it "checks translation" do
-  #   visit root_path
-  #   
-  #   fill_in :translate, with: "Home"
-  #   click_button 'Проверить!'
-  #   expect(page).to have_text("Все верно") 
-  # end
+  it "checks translation" do
+    visit root_path
+   
+    fill_in :translate, with: "Дом"
+    click_button 'Проверить!'
+    expect(page).to have_text("Все верно") 
+  end
   
 end
