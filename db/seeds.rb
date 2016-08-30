26c510855fc6b665ef2bf7654748fe499aaff946
Sergey Atarshchikov <nixkid16@gmail.com>
2016-08-31 01:33:53 +0300

    add user_id for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("http://masterrussian.com/vocabulary/most_common_words.htm"))
+page = Nokogiri::HTML(open('http://masterrussian.com/vocabulary/most_common_words.htm'))
 
 page.css('tr').each do |words|
-  unless (words.css("td")[2]).nil? || (words.css("td")[3]).nil?
-    Card.create!( original_text: words.css("td")[2].content, translated_text: words.css("td")[3].content)
+  unless (words.css('td')[2]).nil? || (words.css('td')[3]).nil?
+    Card.create!(original_text: words.css('td')[2].content, translated_text: words.css('td')[3].content, user_id: User.first.id)
   end
 end
